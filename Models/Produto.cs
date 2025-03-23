0cca9e8c4ff3f15914923ae0c2ec0a0bdf39a5e4
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-22 22:29:30 -0300

    Add project files.

---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O campo {0} deve ser maior que 0")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string? Descricao { get; set; }  
+    }
+}
